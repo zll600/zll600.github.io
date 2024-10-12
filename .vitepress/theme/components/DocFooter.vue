c42832b496ca9f4d61b8a676e8542ccafd08f2cb
zll600 <71714656+zll600@users.noreply.github.com>
2024-10-12 23:12:35 +0800

    feat: use preitter to format code (#58)

--- .vitepress/theme/components/DocFooter.vue
+++ .vitepress/theme/components/DocFooter.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="doc-before-footer-cc-block mt-10 text-sm font-medium">
-    使用 <a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> 许可协议进行共享
+    使用
+    <a
+      class="footer-cc-link"
+      target="_blank"
+      href="https://creativecommons.org/licenses/by-sa/4.0/"
+      >CC BY-SA 4.0</a
+    >
+    许可协议进行共享
   </div>
 </template>
 
--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-  {ignores: ['**/*.md','**/*.yaml','**/*.yml','.vitepress/cache/*'],},
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  { ignores: ["**/*.md", "**/*.yaml", "**/*.yml", ".vitepress/cache/*"] },
+  eslintConfigPrettier,
 ];
